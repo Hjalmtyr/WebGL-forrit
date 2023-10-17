cad4efd99ece6e0596541197f900a16041e42868
Hjalmtyr <hh@hi.is>
2023-10-17 12:44:26 +0000

    Skrár fyrir fyrirlestur 18

--- Angel/waveTepottur.js
+++ Angel/waveTepottur.js
@@ -1,19 +1,12 @@
 /////////////////////////////////////////////////////////////////
 //    Sýnidæmi í Tölvugrafík
-//     Kúla sem lituð er með flatri litun.  Hægt að snúa henni
-//     með músinni og auka/minnka nákvæmni kúlunnar með hnöppum
+//     Tepottur sem litaður er með Phong litun.  Hægt að snúa
+//     honum með músinni og þysja með músarhjóli
 //
 //    Hjálmtýr Hafsteinsson, október 2023
 /////////////////////////////////////////////////////////////////
 var canvas;
 var gl;
-
-var numTimesToSubdivide = 3;
- 
-var index = 0;
-
-var pointsArray = [];
-var normalsArray = [];
 
 var movement = false;     // Do we rotate?
 var spinX = 0;
@@ -21,27 +14,23 @@ var spinY = 0;
 var origX;
 var origY;
 
-var zDist = -5.0;
+var zDist = -4.0;
 
-var fovy = 50.0;
+var fovy = 60.0;
 var near = 0.2;
 var far = 100.0;
 
-var va = vec4(0.0, 0.0, -1.0, 1);
-var vb = vec4(0.0, 0.942809, 0.333333, 1);
-var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-var vd = vec4(0.816497, -0.471405, 0.333333, 1);
-    
-var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightPosition = vec4(10.0, 10.0, 10.0, 1.0 );
+var lightAmbient = vec4(1.0, 1.0, 1.0, 1.0 );
 var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
 var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 
-var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialAmbient = vec4( 0.2, 0.0, 0.2, 1.0 );
 var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
 var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
-var materialShininess = 20.0;
+var materialShininess = 50.0;
 
+var ctm;
 var ambientColor, diffuseColor, specularColor;
 
 var modelViewMatrix, projectionMatrix;
@@ -49,115 +38,76 @@ var modelViewMatrixLoc, projectionMatrixLoc;
 
 var normalMatrix, normalMatrixLoc;
 
+var locTime;
+var iniTime;
+
 var eye;
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
-
-function triangle(a, b, c) {
-
-     var t1 = subtract(b, a);
-     var t2 = subtract(c, a);
-     var normal = normalize(cross(t2, t1));
-     normal = vec4(normal);
-
-     normalsArray.push(normal);
-     normalsArray.push(normal);
-     normalsArray.push(normal);
-     
-     pointsArray.push(a);
-     pointsArray.push(b);      
-     pointsArray.push(c);
-
-     index += 3;
-}
-
-
-function divideTriangle(a, b, c, count) {
-    if ( count > 0 ) {
-                
-        var ab = mix( a, b, 0.5);
-        var ac = mix( a, c, 0.5);
-        var bc = mix( b, c, 0.5);
-                
-        ab = normalize(ab, true);
-        ac = normalize(ac, true);
-        bc = normalize(bc, true);
-                                
-        divideTriangle( a, ab, ac, count - 1 );
-        divideTriangle( ab, b, bc, count - 1 );
-        divideTriangle( bc, c, ac, count - 1 );
-        divideTriangle( ab, bc, ac, count - 1 );
-    }
-    else { 
-        triangle( a, b, c );
-    }
-}
-
-
-function tetrahedron(a, b, c, d, n) {
-    divideTriangle(a, b, c, n);
-    divideTriangle(d, c, b, n);
-    divideTriangle(a, d, b, n);
-    divideTriangle(a, c, d, n);
-}
-
 window.onload = function init() {
 
     canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
-    
-    gl.enable(gl.DEPTH_TEST);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
+    gl.enable(gl.DEPTH_TEST);
+//    gl.enable(gl.CULL_FACE);
+//    gl.cullFace(gl.BACK);
+
+
+    var myTeapot = teapot(15);
+    myTeapot.scale(0.5, 0.5, 0.5);
+
+    console.log(myTeapot.TriangleVertices.length);
+
+    points = myTeapot.TriangleVertices;
+    normals = myTeapot.Normals;
+
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-    
+
 
     ambientProduct = mult(lightAmbient, materialAmbient);
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
     specularProduct = mult(lightSpecular, materialSpecular);
 
-    
-    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
-
-    document.getElementById("Subdivisions").innerHTML = numTimesToSubdivide;
-    document.getElementById("NrVertices").innerHTML = index;
 
     var nBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
-    
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW );
+
     var vNormal = gl.getAttribLocation( program, "vNormal" );
     gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vNormal);
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-    
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
     var vPosition = gl.getAttribLocation( program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-    
+
     modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
     projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
     normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
 
     projectionMatrix = perspective( fovy, 1.0, near, far );
-    
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+
     gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct) );
     gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
+    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );
     gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
     gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess );
+
+    locTime = gl.getUniformLocation( program, "time" );
+    iniTime = Date.now();
 
     //event listeners for mouse
     canvas.addEventListener("mousedown", function(e){
@@ -173,28 +123,12 @@ window.onload = function init() {
 
     canvas.addEventListener("mousemove", function(e){
         if(movement) {
-    	      spinY = ( spinY + (e.clientX - origX) ) % 360;
+    	    spinY = ( spinY + (origX - e.clientX) ) % 360;
             spinX = ( spinX + (origY - e.clientY) ) % 360;
             origX = e.clientX;
             origY = e.clientY;
         }
     } );
-    
-    document.getElementById("btnIncrease").onclick = function(){
-        if( numTimesToSubdivide < 7 ) numTimesToSubdivide++;
-        index = 0;
-        pointsArray = []; 
-        normalsArray = [];
-        init();
-    };
-    document.getElementById("btnDecrease").onclick = function(){
-        if( numTimesToSubdivide > 0 ) numTimesToSubdivide--;
-        index = 0;
-        pointsArray = []; 
-        normalsArray = [];
-        init();
-    };
-
 
     // Event listener for mousewheel
      window.addEventListener("wheel", function(e){
@@ -203,34 +137,32 @@ window.onload = function init() {
          } else {
              zDist -= 0.2;
          }
-     }  );  
+     }  );
 
     render();
 }
 
 
 function render() {
-    
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-    modelViewMatrix = lookAt( vec3(0.0, 0.0, zDist), at, up );
-    modelViewMatrix = mult( modelViewMatrix, rotate( spinY, [0, 1, 0] ) );
-    modelViewMatrix = mult( modelViewMatrix, rotate( spinX, [1, 0, 0] ) );
 
-    // normal matrix only really need if there is nonuniform scaling
-    // it's here for generality but since there is
-    // no scaling in this example we could just use modelView matrix in shaders
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    modelViewMatrix = lookAt( vec3(0.0, 0.0, zDist), at, up );
+    modelViewMatrix = mult( modelViewMatrix, rotateY( -spinY ) );
+    modelViewMatrix = mult( modelViewMatrix, rotateX( spinX ) );
+
     normalMatrix = [
         vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
         vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
         vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
     ];
-            
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
-    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
-        
-    gl.drawArrays( gl.TRIANGLES, 0, index );
 
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
+
+    var msek = Date.now() - iniTime;
+    gl.uniform1f( locTime, msek );
+
+    gl.drawArrays( gl.TRIANGLES, 0, points.length );
     window.requestAnimFrame(render);
 }
